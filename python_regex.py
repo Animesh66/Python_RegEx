cfa35875beae8a541aa8766206c6e644881bb34a
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-12-07 11:46:08 +0530

    Initial Commit

--- python_regex.py
+++ python_regex.py
@@ -1,5 +1,38 @@
 import re
 
+
 search_string = "I am at the Taj hotel"
+
+
 def python_regex():
-    pass
+    # Check if the string starts with "The":
+    x = re.findall("\AThe", search_string)
+    print(x)
+    if x:
+        print("Yes, there is a match!")
+    else:
+        print("No match")
+
+    # Check if "Ta" is present at the beginning of a WORD:
+    y = re.findall(r"\bTa", search_string)
+
+    print(y)
+
+    if y:
+        print("Yes," + y[0] + " is there is in string: " + search_string)
+    else:
+        print("No match")
+
+    # Check if the string has any a, r, or n characters:
+
+    z = re.findall("[tel]", search_string)
+
+    print(z)
+
+    if z:
+        print("Yes, there is at least one match!")
+    else:
+        print("No match")
+
+
+python_regex()
